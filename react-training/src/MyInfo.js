7778416782bf5c474a9a410850285e6e7ec2fa57
Yannai Alter <yanayalter@gmail.com>
2019-04-12 23:02:19 +0300

    Splitted MyInfo to another file - working with files

---
+++ react-training/src/MyInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MyInfo() {
+return (
+    <div>
+        <h1>Its Yannai Alter</h1>
+        <p>This is a paragraph about me</p>
+
+    </div>
+    
+    )
+}
+export default MyInfo
--- react-training/src/index.js
+++ react-training/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import MyInfo from './MyInfo.js';
 function MyApp() {
 return (<ul>
     <li>12</li>
     <li>2</li>
     </ul>)
 }
-ReactDOM.render(<MyApp / >
+ReactDOM.render(<MyInfo / >
     , document.getElementById('root'));
  
